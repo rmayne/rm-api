1936b362016e41260390b8e22c7d06a500728950
rmayne <admin@hypersocialmobile.com>
2015-10-06 12:03:23 -0500

    Create, and update added, Readme added, RMM level 2

--- readme.md
+++ readme.md
@@ -1,3 +1,25 @@
-To start: node rmapi <port>
-resources: api/person, api/person/[id]
+# RM-API
+A very simple REST API made with nodejs
 
+## Installion
+$ git clone https://github.com/rmayne/rm-api.git
+
+## Use
+1. Choose a port number, and start the script
+
+$ node rmpi <port number>
+
+2. The resource should now be available to your REST client, or web browser.
+
+On Ubuntu: localhost:<port number>/api/person
+
+### Additional
+Works well with the client: https://github.com/rmayne/AngularRestClient
+
+### Bugs
+passing in a bad ID causes exit
+
+#### To Do
+* unit tests
+* error messages
+* RMM level 3....
--- rmapi.js
+++ rmapi.js
@@ -1,24 +1,81 @@
 var http = require('http')
 var fs = require('fs')
+var url = require('url')
 
-var server = http.createServer(function(req, res){
+var server = http.createServer(function(req, res) {
 	var responseBody
 
-	if (/^\/api\/person$/.test(req.url)){
-		responseBody = fs.createReadStream('data/person/people.json')
-	} else if (/^\/api\/person\/[0-9]+/.test(req.url)) {
-		id = req.url.match(/[0-9]+$/)
-		responseBody = fs.createReadStream('data/person/' + id + '.json')
+	switch (req.method ){
+
+		// for preflight request
+		case "OPTIONS" :
+			// CORS headers
+		    res.writeHead(200, {
+			    "Access-Control-Allow-Origin" : req.headers.origin	,
+			    "Access-Control-Allow-Methods" : "GET, POST, PUT, OPTIONS",
+			    "Access-Control-Allow-Headers" : "X-Custom-Header, content-type",
+			    "Content-Type": "application/json"
+	    	})
+	    	responseBody = '{"message" : "Success!"}'
+			break
+
+		// for resource list and individual resources
+		case "GET" :
+			// CORS headers
+		    res.writeHead(200, {
+		    	'Access-Control-Allow-Origin' : req.headers.origin,
+		    	'Content-Type': 'application/json'
+	    	})
+	    	//resource list
+			if (/^\/api\/person$/.test(req.url)){
+				responseBody = fs.createReadStream('data/person/people.json')
+			// resource
+			} else if (/^\/api\/person\/[0-9]+/.test(req.url)) {
+				id = req.url.match(/[0-9]+$/)
+				responseBody = fs.createReadStream('data/person/' + id + '.json')
+			}
+			break
+
+		// new resource instance
+		case "POST" :
+			// CORS headers
+		    res.writeHead(200, {
+		    	'Access-Control-Allow-Origin' : req.headers.origin,
+		    	'Content-Type': 'application/json'
+	    	})
+	    	//grabs the query params
+	    	query = url.parse(req.url, true).query
+	    	responseBody = 	'{"id" : 666, "firstname" : "' + query.firstname + '", "lastname" : "' + query.lastname + '", "description" : "' + query.description + '"}'
+			break
+
+		//update resource instance
+		case "PUT" :
+			// CORS headers
+		    res.writeHead(200, {
+		    	'Access-Control-Allow-Origin' : req.headers.origin,
+		    	'Content-Type': 'application/json'
+	    	})
+	    	// could have used the url module, but regex non capture group is so fun!
+			id = req.url.match(/([0-9]+)(?:\?)/)[1]
+			// grab the query params
+	    	query = url.parse(req.url, true).query
+	    	responseBody = 	'{"id" : ' + id + ', "firstname" : "' + query.firstname + '", "lastname" : "' + query.lastname + '", "description" : "' + query.description + '"}'
+			break
 	}
 
+	// flush output
+	// for matched requests
 	if(responseBody){
-		// CORS header included
-	    res.writeHead(200, {'Access-Control-Allow-Origin' : '*', 'Content-Type': 'application/json'
-	    	})
-
-	    responseBody.pipe(res)
+		// for string types
+		if (typeof responseBody == 'string'){
+	    	res.end(responseBody)
+    	// for stream types
+		} else {
+		    responseBody.pipe(res)
+		}
+	// for unmathced requests
 	} else {
-	    res.writeHead(404, {'Access-Control-Allow-Origin' : '*'})
+	    res.writeHead(404, {'Access-Control-Allow-Origin' : req.headers.origin})
 	    res.end('404')
 	}
 })
